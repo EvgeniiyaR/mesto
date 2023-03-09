2b344e92cbc9575fa4c4db743ed453620f1d41a8
EvgeniiyaR <tulpansik@mail.ru>
2023-03-09 23:29:53 +0300

    feature: create card-form

--- script/index.js
+++ script/index.js
@@ -1,12 +1,25 @@
-let body = document.querySelector('.body');
-let profileEditButton = body.querySelector('.profile__edit-button');
-let popup = body.querySelector('.popup');
-let popupInputTypeName = body.querySelector('.popup__input_type_name');
-let popupInputTypeAbout = body.querySelector('.popup__input_type_about');
-let profileName = body.querySelector('.profile__name');
-let profileAbout = body.querySelector('.profile__about');
-let formElement = body.querySelector('.popup__container');
-let popupExitButton = body.querySelector('.popup__exit-button');
+const body = document.querySelector('.body');
+
+const profileEditButton = body.querySelector('.profile__edit-button');
+const profileAddButton = body.querySelector('.profile__add-button');
+
+const popup = body.querySelectorAll('.popup');
+
+const popupTypeEdit = body.querySelector('.popup_type_edit');
+const popupTypeAdd = body.querySelector('.popup_type_add');
+
+const popupInputTypeName = body.querySelector('.popup__input_type_name');
+const popupInputTypeAbout = body.querySelector('.popup__input_type_about');
+const popupInputTypeTitle = body.querySelector('.popup__input_type_title');
+const popupInputTypeUrl = body.querySelector('.popup__input_type_url');
+
+const profileName = body.querySelector('.profile__name');
+const profileAbout = body.querySelector('.profile__about');
+
+const formElement = body.querySelector('.popup__container');
+const popupExitButton = body.querySelectorAll('.popup__exit-button');
+const popupExitButtonArray = Array.from(popupExitButton);
+
 const cardTemplate = body.querySelector('#elements__element').content;
 const listElements = body.querySelector('.elements__list');
 
@@ -37,7 +50,7 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach((item) => addCard(item.name, item.link));
+//Добавление карточек
 
 function addCard(title, imageUrl) {
   const cardElement = cardTemplate.querySelector('.elements__element').cloneNode(true);
@@ -51,25 +64,41 @@ function addCard(title, imageUrl) {
   listElements.append(cardElement);
 }
 
-function openPopup() {
+initialCards.forEach((item) => addCard(item.name, item.link));
+
+//Открытие попапа
+
+function openPopup(popup) {
   popup.classList.add('popup_opened');
+}
+
+function editPopup() {
+  openPopup(popupTypeEdit);
   popupInputTypeName.value = profileName.textContent;
   popupInputTypeAbout.value = profileAbout.textContent;
 }
 
-profileEditButton.addEventListener('click', openPopup);
-
-function exitPopup() {
-  popup.classList.remove('popup_opened');
+function addPopup() {
+  openPopup(popupTypeAdd);
 }
 
-popupExitButton.addEventListener('click', exitPopup);
+profileEditButton.addEventListener('click', editPopup);
+profileAddButton.addEventListener('click', addPopup);
+
+function exitPopup(evt) {
+  const target = evt.target.closest('.popup');
+  target.classList.remove('popup_opened');
+}
+
+//Закрытие попапа
+
+popupExitButtonArray.forEach(item => item.addEventListener('click', exitPopup));
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupInputTypeName.value;
   profileAbout.textContent = popupInputTypeAbout.value;
-  exitPopup();
+  exitPopup(evt);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
